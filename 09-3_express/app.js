59f5952e8b982f8393391c328b456b1b6609ce94
sem201 <sem201@naver.com>
2024-10-12 10:43:31 +0900

    node express

--- 09-3_express/app.js
+++ 09-3_express/app.js
@@ -10,7 +10,7 @@ app.set("views", "./views");
 // static 미들웨어 등록(정적 파일 로드 ex. css, js, img 등)
 // static 이라는 실제 폴더를 public이라는 이름으로 접근하겠다.
 app.use("/public", express.static(__dirname + "/static")); // 위에서처럼 그냥 상대 경로로 해도 된다. (이번에는 __dirname을 이용하여 절대 경로로 넣은것!)
-console.log(__dirname); // C:\Users\sungjae\Desktop\Workspace\codingOn\github\KDT_WEB_14\09-3_express
+console.log(__dirname); // C:\Users\sungjae\Desktop\Workspace\codingOn\github\KDT_WEB_14\09-3_express 실행파일 경로
 
 // app.get(경로, 해당 결로로 들어왔을 떄 실행할 함수)
 // 요청 메서드 : get
@@ -30,7 +30,7 @@ app.get("/", function (req, res) {
   });
 });
 
-// login경로로 요청이 들어오면 로그이 페이지 응답
+// login경로로 요청이 들어오면 로그인 페이지 응답
 app.get("/login", function (req, res) {
   res.render("login");
 });
@@ -38,6 +38,7 @@ app.get("/login", function (req, res) {
 app.get("/register", function (req, res) {
   res.render("register");
 });
+
 app.listen(PORT, function () {
   console.log(`server listenting on ${PORT}! http://localhost:${PORT}`);
 });
---
+++ 10_form/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+//ejs 사용 설정
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// 미들웨어 설정 : 요청과 응답 사이에서 작업을 하는 코드
+// app.use()로 등록
+
+//req.body 객체를 해석할 수 있도록 body-parser 미들웨어 등록 (req.body는 POST 메서드에서 사용)
+
+app.use(express.urlencoded({ extended: true })); //post 요청으로 들어오는 모든 형식의 데이터 파싱
+app.use(express.json()); // json 형식의 데이터 해석, 주고 받게 해줌
+
+// 라우팅(Routing) - 주소 설정
+// ex) GET '/' => index.ejs를 보여줌
+app.get("/", (req, res) => {
+  console.log(req.query);
+  res.render("index");
+});
+
+//GET '/login'으로 들어오는 거 처리
+// get 방식은 클라이언트에서 보낸 데이터(?key=value&key=value..)가 req.query에 저장
+app.get("/login", (req, res) => {
+  console.log(req.query);
+  // res.send("get 요청 성공");
+  // res.render("index"); // (error 한번만 응답 가능하다.)
+
+  res.render("result", { title: "GET 요청", userInfo: req.query });
+});
+
+// POST '/login'
+// post 방식은 클라이언트에서 보낸 데이터가 req.body에 저장
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.render("result", { title: "POST", userInfo: req.body });
+});
+
+app.listen(PORT, () => {
+  console.log(`${PORT}번으로 서버 시작`);
+});
